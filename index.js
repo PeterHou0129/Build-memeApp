5d51a92146bed6375d165684fde449c5047c86a1
PeterHou0129 <206045650+PeterHou0129@users.noreply.github.com>
2025-12-19 05:00:41 +0800

    expert and import data.js

---
+++ index.js
@@ -0,0 +1,24 @@
+import {catsData} from '/data.js'
+
+const emotionRadios = document.getElementById("emotion-radios")
+
+function getEmotionsArray(cats){
+    const emotionArray = []
+    for (let cat of cats){
+        for(let emotion of cat.emotionTags){
+            emotionArray.push(emotion)
+        }
+    }
+    return emotionArray
+}
+
+function renderEmotionsRadios(cats){
+    let radioItems = ``
+    const emotions = getEmotionsArray(cats)
+    for (let emotion of emotions){
+        radioItems += `<p>${emotion}</p>`
+    }
+    emotionRadios.innerHTML = radioItems
+}
+
+renderEmotionsRadios(catsData)
