befdd0677b90dafc9230c8dcae47a3915e27371d
PeterHou0129 <206045650+PeterHou0129@users.noreply.github.com>
2025-12-28 01:13:58 +0800

    updatte

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ import {catsData} from '/data.js'
 
 const emotionRadios = document.getElementById("emotion-radios")
 const getImageBtn = document.getElementById('get-image-btn')
-
+const gifsOnlyOption = document.getElementById('gifs-only-option')
 getImageBtn.addEventListener('click',getMatchingCatsArray)
 emotionRadios.addEventListener('change',highlightCheckedOption)
 
 function getMatchingCatsArray(){
+    const isGif = gifsOnlyOption.checked
+
     if(document.querySelector('input["type=radio"]:checked')){
         const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
-        console.log(selectedEmotion)
+        const isGif = gifsOnlyOption.checked
+        const matchingCatsArray = catsData.filter(function(cat){
+            return cat.emotionTags.includes(selectedEmotion)
+        })
     }
 }
 
